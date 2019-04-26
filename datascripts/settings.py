b3b6b08fde21ba89640e4a1f76a9e27c194b2ed7
syed muhammad hassan <syedmuhammadhassan1990@gmail.com>
2019-04-26 18:17:06 +0500

    second_commit

--- datascripts/settings.py
+++ datascripts/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '2a-za$s5l9*2f$!+f4p(yf5vjr)frqh&4ik6oj5v6!8rtcxz(+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://datascripts.herokuapp.com/']
 
 
 # Application definition
@@ -125,3 +126,5 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'datascripts/assets')
 ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# Activate Django-Heroku.
+django_heroku.settings(locals())
